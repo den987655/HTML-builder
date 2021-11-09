2d9194298a696e575e1847b25b7f50b16ce9583c
den987655 <den987655@gmail.com>
2021-11-09 18:57:42 +0300

    feat: add task 4

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,23 +1,29 @@
-
-
-
 const fs = require('fs');
 const path = require('path');
 
 
+fs.mkdir('./04-copy-directory/files-copy', () => {
+  fs.readFile('./files', 'utf8', () => {
 
-fs.mkdir(path.join(__dirname, 'files-copy'), (err) => {
-  if (err) throw err;
 
-  console.log('создал папку');
-  
+fs.copyFile(path.join(__dirname,'files/test-css.css',), path.join(__dirname,'files-copy/test-css.css'), err => {
+if(err) throw err; // не удалось скопировать файл
+console.log('Файл успешно скопирован');
+});
+fs.copyFile(path.join(__dirname,'files/test-image.jpg',), path.join(__dirname,'files-copy/test-image.jpg'), err => {
+if(err) throw err; // не удалось скопировать файл
+console.log('Файл успешно скопирован');
+});
+fs.copyFile(path.join(__dirname,'files/test-js.js',), path.join(__dirname,'files-copy/test-js.js'), err => {
+if(err) throw err; // не удалось скопировать файл
+console.log('Файл успешно скопирован');
+});
+fs.copyFile(path.join(__dirname,'files/test-text.txt',), path.join(__dirname,'files-copy/test-text.txt'), err => {
+if(err) throw err; // не удалось скопировать файл
+console.log('Файл успешно скопирован');
 });
 
-fs.copyFile('files/test-css.css', 'files-copy/test-css.css', err => {
-   if(err) throw err; // не удалось скопировать файл
-   console.log('Файл успешно скопирован');
 });
-/* fs.copyFile('files/test-css.css', 'files-copy/test-css.css', err => {
-  if(err) throw err; // не удалось переместить файл
-  console.log('Файл успешно перемещён');
-}); */
+
+});
+
